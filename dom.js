73fe3ea722c7fd601ce4eb3f4ce22dfb97a08e35
priyabanait <priyabanait91@gmail.com>
2023-01-31 00:42:57 +0530

    DOM js file

---
+++ dom.js
@@ -0,0 +1,6 @@
+
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000';
+var item=document.getElementsByClassName('title');
+item[0].style.fontWeight='bold';
+item[0].style.color='green';

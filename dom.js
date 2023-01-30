49cf8860e9ef972362481004e8176606716f6631
priyabanait <priyabanait91@gmail.com>
2023-01-31 01:32:54 +0530

    GetElementsbyClassname added

--- dom.js
+++ dom.js
@@ -4,3 +4,9 @@ header.style.borderBottom='solid 3px #000';
 var item=document.getElementsByClassName('title');
 item[0].style.fontWeight='bold';
 item[0].style.color='green';
+var item2=document.getElementsByClassName('list-group-item');
+item2[2].style.backgroundColor='green';
+for(let i=0;i<item2.length;i++){
+    item2[i].style.fontWeight='bold';
+    item2[i].style.color='yellow';
+}

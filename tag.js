c780154f9a9172ecada2d5a86dd2abf2b3473fcb
priyabanait <priyabanait91@gmail.com>
2023-01-31 15:21:10 +0530

    GetElementsbyTAgName added

---
+++ tag.js
@@ -0,0 +1,18 @@
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000';
+var item=document.getElementsByClassName('title');
+item[0].style.fontWeight='bold';
+item[0].style.color='green';
+var item2=document.getElementsByClassName('list-group-item');
+item2[2].style.backgroundColor='green';
+for(let i=0;i<item2.length;i++){
+    item2[i].style.fontWeight='bold';
+    item2[i].style.color='yellow';
+}
+
+var item3=document.getElementsByClassName('group-item');
+item3[0].style.fontWeight='bold';
+item3[0].style.color='blue';
+
+var item4=document.getElementsByTagName('li');
+item4[4].style.backgroundColor='black'

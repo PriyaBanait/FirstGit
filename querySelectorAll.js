800ba6409c88155b2adcaf5a0281d98afd626e1e
priyabanait <priyabanait91@gmail.com>
2023-01-31 16:38:20 +0530

    QuerySelectorALL added

---
+++ querySelectorAll.js
@@ -0,0 +1,7 @@
+const nodeList = document.querySelectorAll("li.list-group-item");
+nodeList[1].style.color = "green"; 
+
+const odd=document.querySelectorAll('li:nth-child(odd');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
+}

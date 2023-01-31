b07d56cf00014e848bcc068baf8347a35b4322c8
priyabanait <priyabanait91@gmail.com>
2023-01-31 16:16:43 +0530

    QuerySelector added

---
+++ queryselector.js
@@ -0,0 +1,8 @@
+
+//querySelector
+
+var item5=document.querySelector('.list-group-item:nth-child(2)');
+item5.style.backgroundColor='green';
+
+var item6=document.querySelector('.list-group-item:nth-child(3)');
+item6.style.display = 'none'
